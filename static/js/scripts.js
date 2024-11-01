f72f28ade7691abccad8c2e0822ed0d7fe15f512
Francerfernance <thekidofsteel01@gmail.com>
2024-11-01 19:42:03 -0400

    Make Contact information disappear

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-* Start Bootstrap - Clean Blog v6.0.9 (https://startbootstrap.com/theme/clean-blog)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
-*/
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
     const mainNav = document.getElementById('mainNav');
@@ -30,4 +25,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Set the current year
     document.getElementById("year").textContent = new Date().getFullYear();
+
+    // Reset form if the message has been sent
+    const msgSent = {{ msg_sent | tojson }};
+    if (msgSent) {
+        document.querySelector('form').reset();
+    }
 });
